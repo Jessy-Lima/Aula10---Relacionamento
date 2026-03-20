ce8714e442a6e39dc6fca92bd2201a87448dc08d
Jessica Lima <maytajessica73@gmail.com>
2026-03-20 14:48:06 -0300

    criando a classe e adicionando valores

--- main.py
+++ main.py
@@ -1,5 +1,63 @@
-#SQLAALCHEMY + ORM
-#pip install sqlalchemy
-
+# SQLALCHEMY + ORM
+# pip install sqlalchemy
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+Base = declarative_base()
+
+# Tabelas do banco
+class Usuario(Base):
+    __tablename__ = "usuarios"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+
+    #relacionamento
+    pedidos = relationship("Pedido", back_populates="usuario")
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    #Função para imprimir
+    def __repr__(self):
+        return f"Usuario =  id={self.id} - nome={self.nome}"
+
+class Pedido(Base):
+    __tablename__ = "pedidos"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    produto = Column(String(100), nullable=False)
+
+    #Chave estrangeira
+    # Onde tem o foreing key, tem o relacionamento muitos para um (muitos pedidos para um usuario)
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"))
+
+    # Relacionamento
+    usuario = relationship("Usuario", back_populates="pedidos")
+
+    def __init__(self, produto):
+        self.produto = produto
+
+    def __repr__(self):
+        return f"Pedido =  id={self.id} - Produto={self.produto}"
+    
+engine = create_engine("sqlite:///loja.db")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+
+with Session() as session:
+    #Criando um objeto
+    usuario1 = Usuario("Joel")
+
+    #Criando os pedidos
+    pedido1 = Pedido("Celular")
+    pedido2 = Pedido("Caneta")
+    pedido3 = Pedido("Livro")
+
+    #Associando pedidos aos usuarios
+    usuario1.pedidos.append(pedido1)
+    usuario1.pedidos.append(pedido2)
+    usuario1.pedidos.append(pedido3)
+
+    #Salvar no banco
+    session.add(usuario1)
+    session.commit()
