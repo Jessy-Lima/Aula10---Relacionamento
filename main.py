6bbeee50aaa5ff0fd350342190a5f8cb0ee73bff
Jessica Lima <maytajessica73@gmail.com>
2026-03-20 13:56:27 -0300

    usando from e import

---
+++ main.py
@@ -0,0 +1,5 @@
+#SQLAALCHEMY + ORM
+#pip install sqlalchemy
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
